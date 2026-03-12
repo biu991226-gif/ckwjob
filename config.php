fc6b6daccbe9d0e639fabff7267af9a28cb2f12c
biu991226-gif <biu991226@hotmail.com>
2026-03-12 22:04:15 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,23 +1,23 @@
 <?php
 
 // ローカル環境（有効）
+// return [
+//     "db_host" => "localhost",
+//     "db_port" => 3306,
+//     "db_name" => "mzx991226_zhixin",
+//     "db_user" => "mzx991226_zhixin",
+//     "db_pass" => "mzx991226",
+//     "db_charset" => "utf8mb4",
+// ];
+
+
+// 本番環境（デプロイ時に有効化）
 return [
-    "db_host" => "localhost",
+    "db_host" => "43.206.154.211",
     "db_port" => 3306,
     "db_name" => "mzx991226_zhixin",
-    "db_user" => "mzx991226_zhixin",
-    "db_pass" => "mzx991226",
+    "db_user" => "user",
+    "db_pass" => "Password123!",
     "db_charset" => "utf8mb4",
 ];
 
-/*
-// 本番環境（デプロイ時に有効化）
-return [
-    "db_host" => "YOUR_PROD_DB_HOST",
-    "db_port" => 3306,
-    "db_name" => "YOUR_PROD_DB_NAME",
-    "db_user" => "YOUR_PROD_DB_USER",
-    "db_pass" => "YOUR_PROD_DB_PASSWORD",
-    "db_charset" => "utf8mb4",
-];
-*/
